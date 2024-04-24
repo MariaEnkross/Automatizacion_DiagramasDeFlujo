f75780ba0a1adc74ddb711c0c48f9cdd8bf5577a
mariaEnkross <leon.m@enkross.com>
2024-04-24 08:44:58 +0200

    correciones2

--- prueba_errores1.py
+++ prueba_errores1.py
@@ -4,7 +4,9 @@ import numpy as np
 vector=["IME1","W1","IME2","W2","IME3"]
 grafo=vector[:]
 tam=len(vector)
+
 #cuenta de la línea dentro de la página
+
 linea=0
 for i in range(tam):
 		grafo[i]=str(vector[i]) + ", [" + str(i*10) + ","+ str(linea)+"],"
