1d6f11eb25c632dd80889c3563478198661854bb
mariaEnkross <leon.m@enkross.com>
2024-04-24 07:52:20 +0200

    correcion formato horas

--- prueba3_Automatizacion_Excel.py
+++ prueba3_Automatizacion_Excel.py
@@ -2,23 +2,27 @@ import openpyxl
 from datetime import datetime
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
-from reportlab.lib.pagesizes import landscape, A3, A4
+from reportlab.lib.pagesizes import landscape, A4
 
 # Abre el archivo Excel lee solo los valores resultantes (ignorando las fórmulas)
 wb = openpyxl.load_workbook('excel.xlsx', data_only=True)
-sheet = wb['Tiempos']  # Se puede acceder a una hoja específica si es necesario
+sheet = wb['Organización']  # Se puede acceder a una hoja específica si es necesario
+
+# Imprimir los nombres de todas las hojas en el archivo Excel
+print("La hojas de este Excel son: ", wb.sheetnames)
 
 # Lee datos de la hoja Excel y los almacena en una lista
 data = []
 for row in sheet.iter_rows(values_only=True):
     filtered_row = [value.strftime('%d-%m-%Y') 
                     if isinstance(value, datetime) 
-                        else value for value in row if value is not None]  # Formatear fechas y filtrar celdas en blanco
+                        else round(value) if isinstance(value, (int, float)) 
+                        else value for value in row if value is not None]  # Formatear fechas, redondear horas y filtrar celdas en blanco
     if filtered_row:  # Comprueba si filtered_row contiene algún valor despues del filtrado
         data.append(filtered_row)
 
 # Crear un documento PDF
-pdf_filename = "prueba3_datos_excel.pdf"
+pdf_filename = "prueba3_datos_excel.pdf" 
 doc = SimpleDocTemplate(pdf_filename, pagesize=landscape(A4))
 
 # Crear una tabla con los datos
@@ -39,3 +43,4 @@ elements = [table]
 doc.build(elements)
 
 print(f"El PDF ha sido generado correctamente")
+
