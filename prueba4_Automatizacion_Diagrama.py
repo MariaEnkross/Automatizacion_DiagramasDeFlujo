e2fd782410ceab8266ed50c001732b657ebf4a36
mariaEnkross <leon.m@enkross.com>
2024-04-25 13:32:10 +0200

    correcion rombos/cuadrados en nodos

--- prueba4_Automatizacion_Diagrama.py
+++ prueba4_Automatizacion_Diagrama.py
@@ -22,16 +22,18 @@ pos = {}  # posición
 x_position = 0
 y_position = 0
 for node in G.nodes():
-    if x_position == 1 or x_position == 3:  # Si es el primer nodo o el tercer nodo, dibujamos un rombo
-        pos[node] = (x_position, y_position)  # Posición para el primer nodo
+    if x_position == len(G.nodes()) - 2:  # Si es el penúltimo nodo
+        pos[node] = (x_position - 4, - 1)  # Colocar el penúltimo nodo en la posición (-4, -1)
         nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=3000, node_shape='d', node_color='skyblue', label=node)
+    elif x_position == len(G.nodes()) - 1:  # Si es el último nodo
+        pos[node] = (x_position - 4, - 1)  # Colocar el último nodo en la posición (-3, -1)
+        nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=3000, node_shape='s', node_color='lightgreen', label=node)
     else:
-        if x_position < len(G.nodes()) - 2:  # Si es un nodo distinto al primero o al tercero, dibujamos un rectángulo
-            pos[node] = (x_position, y_position)  # Posición para todos los nodos excepto los dos últimos
-            nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=3000, node_shape='s', node_color='lightgreen', label=node)
-        else:  # Si es uno de los dos últimos nodos, movidos hacia la izquierda y hacia abajo
-            pos[node] = (x_position - 4, - 3)
-            y_position = 0.5
+        if x_position == 1 or x_position == 3:  # Si es el primer nodo o el tercer nodo, dibujamos un rombo
+            pos[node] = (x_position, y_position)  # Posición para el primer nodo
+            nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=3000, node_shape='d', node_color='skyblue', label=node)
+        else:  # Si es cualquier otro nodo, dibujamos un rectángulo
+            pos[node] = (x_position, y_position)
             nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=3000, node_shape='s', node_color='lightgreen', label=node)
     x_position += 1
 
