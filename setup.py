5e881fc6a15cead948e059af0bb3dd9df100de1d
mariaEnkross <leon.m@enkross.com>
2024-05-07 09:50:00 +0200

    ejecutable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="Desarrollo Unifilares",
+    version="1.0",
+    description="Executable Python",
+    executables=[Executable("prueba7_Ejecutable.py")]
+)
